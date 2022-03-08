0c15faab9f7f13db5560493747f0fc956d08455c
Jovan Miladinovic <jovanmiladinovice14@gmail.com>
2022-03-08 15:59:20 +0100

    kraj

--- get-quote.py
+++ get-quote.py
@@ -2,12 +2,15 @@ import random
 def p():
     #print("Keep it logically awesome.")
 
-    f = open("quotes.txt")
+    f = open("quotes.txt","a")
+    f.write("Get busy living or get busy dying")
     quotes = f.readlines()
     f.close()
-    last = 13
+    last = len(quotes)
     rnd = random.randint(0,last)
-    print(quotes[rnd])
+    print(quotes[rnd],end = "")
+    print(quotes[15])
+
 
 if __name__== "__main__":
   p()
